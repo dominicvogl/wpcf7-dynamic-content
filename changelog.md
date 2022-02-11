2503fb5b787e235b25390f3d67936c2708d2254f
Dominic Vogl <dv@cat-ia.de>
2022-02-11 17:30:15 +0100

    add new function to reformat line breaks

--- changelog.md
+++ changelog.md
@@ -1,5 +1,8 @@
 # Changelog
 
+[0.0.2] Add line breaks
+* add function to replace line breaks characters to <br>
+
 [0.0.1] First Version
 * init with class, and filtering the post content variable and adding the acf data
 * transfer debugging
--- wp-contact-form-7-dc.php
+++ wp-contact-form-7-dc.php
@@ -6,9 +6,8 @@ Author: Dominic Vogl
 Author URI: https://github.com/dominicvogl/
 Text Domain: contact-form-7-dc
 Domain Path: /languages/
-Version: 0.0.1
+Version: 0.0.2
 */
-
 
 
 // if this file is called directly, DIE!
@@ -28,46 +27,72 @@ class Cf7_Add_Dynamic_Value {
 	//main constructor
 	public function __construct() {
 		add_filter( 'wpcf7_posted_data', array( $this, 'add_dynamic_data' ) );
+		add_filter( 'wpcf7_mail_tag_replaced', array( $this, 'format_line_breaks' ), 10, 4 );
 	}
+
 
 	/**
 	 * @hooked wpcf7_posted_data
+	 *
 	 * @param array $posted_data
+	 *
+	 * @since 0.0.1
+	 * @version 0.0.2
+	 * @author Dominic Vogl
 	 */
+
 	public function add_dynamic_data( $posted_data ) {
 
-		$quote = '';
-		$your_name = isset( $posted_data[ 'company-name' ] ) ? sanitize_text_field( $posted_data[ 'company-name' ]  ) : '';
-		$your_email = isset( $posted_data[ 'client-id' ] ) ? sanitize_text_field( $posted_data[ 'client-id' ]  ) : '';
-		$your_subject = isset( $posted_data[ 'yourmail' ] ) ? sanitize_text_field( $posted_data[ 'yourmail' ]  ) : '';
-		$radios = isset( $posted_data[ 'radio-147-0' ] ) ? sanitize_text_field( $posted_data[ 'radio-147-0' ]  ) : '';
-		$radios = isset( $posted_data[ 'radio-[values]' ] ) ? sanitize_text_field( $posted_data[ 'radio-147-1' ]  ) : '';
-		//you can fillup this value as per your requirement. Replace dynami-content with the hidden name added on your form
-
 		// collect Radio results for returning
-		if(isset($posted_data['radio'])) {
+		if ( isset( $posted_data['radio'] ) ) {
 			$radioResults = [];
-			foreach($posted_data['radio']['values'] as $radio) {
+			foreach ( $posted_data['radio']['values'] as $radio ) {
 				$radioResults[] = $radio[0];
 			}
 		}
 
-		if(isset($posted_data['checkbox'])) {
+		if ( isset( $posted_data['checkbox'] ) ) {
 			// collecting checkbox results
 			$checkboxResults = [];
-			foreach($posted_data['checkbox']['values'] as $checkboxGroup) {
-				foreach($checkboxGroup as $checkboxValue) {
+			foreach ( $posted_data['checkbox']['values'] as $checkboxGroup ) {
+				foreach ( $checkboxGroup as $checkboxValue ) {
 					$checkboxResults[] = $checkboxValue;
 				}
 			}
 		}
 
-		$posted_data[ 'dynamic-radios' ] = implode('; ', $radioResults);
-		$posted_data[ 'dynamic-checkboxes'] = implode('; ', $checkboxResults);
+		$posted_data['dynamic-radios']     = implode( ';\n', $radioResults );
+		$posted_data['dynamic-checkboxes'] = implode( ';\n ', $checkboxResults );
 
-		$posted_data['dynamic-debug'] = json_encode($posted_data);
+		$posted_data['dynamic-debug'] = json_encode( $posted_data );
 
 		return $posted_data;
+	}
+
+	/**
+	 * @param $replaced
+	 * @param $submitted
+	 * @param $html
+	 * @param $mail_tag
+	 *
+	 * @hooked wpcf7_mail_tag_replaced
+	 *
+	 * transform break signs from dynamic fields above to html line breaks
+	 *
+	 * @return array|mixed|string|string[]
+	 * @since 0.0.2
+	 * @author Dominic Vogl
+	 */
+
+	public function format_line_breaks( $replaced, $submitted, $html, $mail_tag ) {
+
+		$fields = [ 'dynamic-checkboxes', 'dynamic-radios' ];
+
+		if ( in_array( $mail_tag->field_name(), $fields ) && $html ) {
+			$replaced = str_replace( ';\n', '<br>', $submitted );
+		}
+
+		return $replaced;
 	}
 
 
